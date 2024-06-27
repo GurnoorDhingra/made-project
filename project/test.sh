5adc9183710ebd38ae134716b94af0276d4a537f
GurnoorDhingra <gurnoorkaurdhingra@gmail.com>
2024-06-27 11:29:59 +0200

    project work6

--- project/test.sh
+++ project/test.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+echo ">>> installing required packages..."
 pip install pandas
 pip install numpy
 pip install requests
 pip install sqlalchemy
 pip install unittest2
 
-python ./automated_test.py
+echo ">>> running the tests ..."
+if python ./automated_test.py; then 
+  echo ">>> tests ran successfully..."
+else 
+  echo ">>> tests failed..."
+fi
